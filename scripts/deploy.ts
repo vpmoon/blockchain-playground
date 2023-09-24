c76f96fd14aa3c99b7d0338420fae20cef446214
vpetrenko <vika.petrenko.external@moonfare.com>
2023-09-24 19:22:07 +0200

    feat(contract): unregister func

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,22 +1,9 @@
 import { ethers } from "hardhat";
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
+  const contract = await ethers.deployContract("RegisterDomains");
 
-  const lockedAmount = ethers.parseEther("0.001");
-
-  const lock = await ethers.deployContract("Lock", [unlockTime], {
-    value: lockedAmount,
-  });
-
-  await lock.waitForDeployment();
-
-  console.log(
-    `Lock with ${ethers.formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.target}`
-  );
+  await contract.waitForDeployment();
 }
 
 // We recommend this pattern to be able to use async/await everywhere
--- test/RegisterDomains.ts
+++ test/RegisterDomains.ts
@@ -4,6 +4,8 @@ const {
 } = require("@nomicfoundation/hardhat-toolbox/network-helpers");
 const { ethers } = require("hardhat");
 
+const undefinedAddress = "0x0000000000000000000000000000000000000000";
+
 describe("RegisterDomains contract", function () {
     async function deployTokenFixture() {
         const [owner, addr1, addr2] = await ethers.getSigners();
@@ -33,24 +35,15 @@ describe("RegisterDomains contract", function () {
         });
 
         it("Should fail if not enough etn for registering domain", async function () {
-            const { hardhatToken, owner, addr1, addr2 } = await loadFixture(deployTokenFixture);
+            const { hardhatToken, owner, addr1 } = await loadFixture(deployTokenFixture);
 
             expect(await hardhatToken.owner()).to.equal(owner.address);
 
             const etherToSend = ethers.parseEther("0.1");
 
-            // await expect(lock.withdraw()).to.be.revertedWith( //TODO try this
-            //     "You can't withdraw yet"
-            // );
-
-            try {
-                await hardhatToken.connect(addr1).registerDomain('com', { from: addr1, value: etherToSend });
-                // If the transaction succeeds, it's an error
-                assert.fail("Transaction should have failed");
-            } catch (error) {
-                // Check if the error message matches the expected message
-                assert.include(error.message, "Insufficient ETH sent");
-            }
+            await expect(hardhatToken.connect(addr1)
+                .registerDomain('com', { from: addr1, value: etherToSend }))
+                .to.be.revertedWith("Insufficient ETH sent");
         });
 
         it("Should take deposit as 1 ether when assign domain", async function () {
@@ -61,16 +54,27 @@ describe("RegisterDomains contract", function () {
             await expect(tx).to.changeEtherBalances([addr1, hardhatToken], [-ether, ether]);
         });
 
-        // it("Should unregister domain", async function () {
-        //     const { hardhatToken, owner, addr1 } = await loadFixture(deployTokenFixture);
-        //
-        //     const etherToSend = ethers.parseEther("1");
-        //
-        //     await hardhatToken.connect(addr1).unregisterDomain('com');
-        //     const domainDetails = await hardhatToken.getDomainOwner('com');
-        //
-        //     expect(domainDetails.domainOwner).to.equal(addr1.address);
-        // });
+        it("Should fail if unregistering by not owner", async function () {
+            const { hardhatToken, addr1, addr2, ether } = await loadFixture(deployTokenFixture);
+
+            await hardhatToken.connect(addr1).registerDomain('com', { from: addr1, value:  ether });
+
+            await expect(hardhatToken.connect(addr2)
+                .unregisterDomain('com', { from: addr2, value: ether }))
+                .to.be.revertedWith("Domain should be unregistered by the domain owner");
+        });
+
+        it("Should unregister domain and return deposit", async function () {
+            const { hardhatToken, addr1, ether } = await loadFixture(deployTokenFixture);
+
+            await hardhatToken.connect(addr1).registerDomain('com', { from: addr1, value:  ether });
+            const tx = await hardhatToken.connect(addr1).unregisterDomain('com');
+
+            const domainDetails = await hardhatToken.getDomainOwner('com');
+
+            expect(domainDetails.domainOwner).to.equal(undefinedAddress)
+            await expect(tx).to.changeEtherBalances([addr1, hardhatToken], [ether, -ether]);
+        });
     });
 
 });
