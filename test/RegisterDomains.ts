2b40b7ad8c28812e025e735585d2e468d274d72c
vpetrenko <vika.petrenko.external@moonfare.com>
2023-09-28 19:34:26 +0200

    feat(task 4): renames

--- test/RegisterDomains.ts
+++ test/RegisterDomains.ts
@@ -8,82 +8,82 @@ import { AddressZero } from "@ethersproject/constants";
 describe("RegisterDomains contract", function () {
     async function deployTokenFixture() {
         const [owner, addr1] = await ethers.getSigners();
-        const hardhatToken = await ethers.deployContract("RegisterDomains");
+        const domainsContract = await ethers.deployContract("RegisterDomains");
         const ether = ethers.parseEther("1");
 
-        await hardhatToken.waitForDeployment();
+        await domainsContract.waitForDeployment();
 
-        return { hardhatToken, owner, addr1, ether };
+        return { domainsContract, owner, addr1, ether };
     }
 
     describe("Deployment", function () {
         describe('Initialization', function () {
             it("Should set owner correctly", async function () {
-                const { hardhatToken, owner } = await loadFixture(deployTokenFixture);
+                const { domainsContract, owner } = await loadFixture(deployTokenFixture);
 
-                expect(await hardhatToken.owner()).to.equal(owner.address);
+                expect(await domainsContract.owner()).to.equal(owner.address);
             });
         });
 
         describe('Domain registration', function () {
             it("Should register domain", async function () {
-                const { hardhatToken, owner } = await loadFixture(deployTokenFixture);
+                const { domainsContract, owner } = await loadFixture(deployTokenFixture);
 
                 const etherToSend = ethers.parseEther("1");
 
-                await hardhatToken.registerDomain('com', { value:  etherToSend });
+                await domainsContract.registerDomain('com', { value:  etherToSend });
 
-                const domainDetails = await hardhatToken.getDomain('com');
+                const domainDetails = await domainsContract.getDomain('com');
 
                 expect(domainDetails.controller).to.equal(owner.address);
                 expect(domainDetails.deposit).to.equal(etherToSend);
             });
 
             it("Should fail if not enough etn for registering domain", async function () {
-                const { hardhatToken } = await loadFixture(deployTokenFixture);
+                const { domainsContract } = await loadFixture(deployTokenFixture);
 
                 const etherToSend = ethers.parseEther("0.1");
 
-                await expect(hardhatToken.registerDomain('com', { value: etherToSend }))
+                await expect(domainsContract.registerDomain('com', { value: etherToSend }))
                     .to.be.revertedWith("Insufficient ETH sent");
             });
 
             it("Should take deposit as 1 ether when assign domain", async function () {
-                const { hardhatToken, owner, ether } = await loadFixture(deployTokenFixture);
+                const { domainsContract, owner, ether } = await loadFixture(deployTokenFixture);
 
-                const tx = await hardhatToken.registerDomain('com', { value: ether });
+                const tx = await domainsContract.registerDomain('com', { value: ether });
 
-                await expect(tx).to.changeEtherBalances([owner, hardhatToken], [-ether, ether]);
+                await expect(tx).to.changeEtherBalances([owner, domainsContract], [-ether, ether]);
             });
         });
 
         describe('Domain releasing', function () {
             it("Should unregister domain and return deposit", async function () {
-                const { hardhatToken, owner, ether } = await loadFixture(deployTokenFixture);
+                const { domainsContract, owner, ether } = await loadFixture(deployTokenFixture);
 
-                await hardhatToken.registerDomain('com', { value:  ether });
-                const tx = await hardhatToken.unregisterDomain('com');
+                await domainsContract.registerDomain('com', { value:  ether });
+                const tx = await domainsContract.unregisterDomain('com');
 
-                const domainDetails = await hardhatToken.getDomain('com');
+                const domainDetails = await domainsContract.getDomain('com');
 
                 expect(domainDetails.controller).to.equal(AddressZero)
-                await expect(tx).to.changeEtherBalances([owner, hardhatToken], [ether, -ether]);
+                await expect(tx).to.changeEtherBalances([owner, domainsContract], [ether, -ether]);
             });
 
             it("Should fail if unregistering by not owner", async function () {
-                const { hardhatToken, addr1, ether } = await loadFixture(deployTokenFixture);
+                const { domainsContract, addr1, ether } = await loadFixture(deployTokenFixture);
 
-                await hardhatToken.registerDomain('com', { value:  ether });
+                await domainsContract.registerDomain('com', { value:  ether });
 
-                await expect(hardhatToken.connect(addr1)
+                await expect(domainsContract.connect(addr1)
                     .unregisterDomain('com', { value: ether }))
                     .to.be.revertedWith("Domain should be unregistered by the domain owner");
             });
 
             it("Should fail if unregistering free domain", async function () {
-                const { hardhatToken, ether } = await loadFixture(deployTokenFixture);
+                const { domainsContract, ether } = await loadFixture(deployTokenFixture);
 
-                await expect(hardhatToken
+                await expect(domainsContract
                     .unregisterDomain('ua', { value: ether }))
                     .to.be.revertedWith("Domain is not registered yet");
             });
