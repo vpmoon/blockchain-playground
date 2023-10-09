fa34ee21e83bfb54a8f28be483185fc8b4c32fc2
vpetrenko <vika.petrenko.external@moonfare.com>
2023-10-10 00:57:49 +0200

    clean

--- contracts/StringParserAssemblyLibrary.sol
+++ contracts/StringParserAssemblyLibrary.sol
@@ -4,20 +4,15 @@ pragma solidity ^0.8.17;
 library StringParserAssemblyLibrary {
 
     function stripAfter(string memory input, string memory symbol) public pure returns (string memory) {
-        // Get the length of the input string
         uint256 inputLength;
         assembly {
             inputLength := mload(input)
         }
-
-        // Get the length of the symbol
         uint256 symbolLength;
         assembly {
             symbolLength := mload(symbol)
         }
-
-        // Search for the symbol in the input string
-        uint256 symbolIndex = inputLength; // Initialize with input's length to indicate not found
+        uint256 symbolIndex = inputLength;
         for (uint256 i = 0; i <= inputLength - symbolLength; i++) {
             bool found = true;
             for (uint256 j = 0; j < symbolLength; j++) {
@@ -37,20 +32,13 @@ library StringParserAssemblyLibrary {
                 break;
             }
         }
-
-        // If the symbol is not found, return the entire input string
         if (symbolIndex == inputLength) {
             return input;
         }
-
-        // Calculate the length of the substring after the symbol
         uint256 substringLength = inputLength - symbolIndex - symbolLength;
-
-        // Create a new string for the substring
         string memory substring = new string(substringLength);
         bytes memory substringBytes = bytes(substring);
 
-        // Copy the substring into the new string
         for (uint256 k = 0; k < substringLength; k++) {
             uint256 inputChar;
             assembly {
