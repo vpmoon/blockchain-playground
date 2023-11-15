65f6bd135eab18c00a865ce9135a446071a28522
vpmoon <vika.petrenko.external@moonfare.com>
2023-11-15 01:21:39 +0000

    deploy

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,6 +1,9 @@
 const { ethers } = require("hardhat");
 
 async function main() {
+    const domainTokenFactory = await ethers.getContractFactory("contracts/DomainToken.sol:DomainToken");
+    const domainTokenContract = await domainTokenFactory.deploy(20000);
+
     const stringParserLibrary = await ethers.getContractFactory("contracts/StringParserLibrary.sol:StringParserLibrary");
     const stringParserLibraryDeployed = await stringParserLibrary.deploy();
 
@@ -19,6 +22,10 @@ async function main() {
     const proxyContract = await upgrades.deployProxy(domainRegistry, {
         initializer: "initialize",
         unsafeAllowLinkedLibraries: true,
+        args: [
+            await domainTokenContract.getAddress(),
+            0x694AA1769357215DE4FAC081bf1f309aDC325306,
+        ],
     });
     console.log("DomainRegistryV1 deployed to:", await proxyContract.getAddress());
 }
